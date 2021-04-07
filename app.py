6862d9bd5dcd6c2f4ffedceb3e6bb1131b86ad1c
tonyarris <ash552@york.ac.uk>
2021-04-07 06:04:18 +0200

    Add jquery handler on index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -8,6 +8,13 @@ Bootstrap(app)
 def home():
     if request.method == 'GET':
         return render_template('index.html')
+
+@app.route('/query',methods=['POST'])
+def query():
+    query = request.form.to_dict()
+    print("Your query was ".format(query))
+    response = query(query)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
