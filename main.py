b3e52da1d177afc26ebdbe93cf899a577e286653
tonyarris <ash552@york.ac.uk>
2021-04-10 07:36:53 +0200

    Adjust bootstrap layout.

--- main.py
+++ main.py
@@ -27,15 +27,11 @@ def gpt(prompt):
         r = r.replace('\\n','')
         r = json.loads(r)
         r = json.dumps(r["choices"][0]["text"])
-        # prototyping string sanification
-        test_res = '{"id": "cmpl-2l2DPoXWLq88CVKW3Kk0oC0PQURPr", "object": "text_completion", "created": 1617562607, "model": "davinci:2020-05-03", "choices": [{"text": "\\n\\n<div style=\\"background-color:red;\\">\\n\\n</div>\\n\\n<div style=\\"background-color:blue;\\">\\n\\n</div>\\n\\n<div style=\\"background-color:green;\\">\\n\\n</div>\\n\\n<div style=\\"background-color:yellow;\\">\\n\\n</div>\\n\\n<div style=\\"background-color:orange;\\">\\n\\n</div>\\n\\n<div style=\\"background-color:", "index": 0, "logprobs": null, "finish_reason": "length"}]}'
-        # strip escape characters
-        test_res = test_res.replace('\\n', '')
-        #test_res = test_res.replace('\\', '')
-        print(test_res)
-        test_res = json.loads(test_res)
-        test_res = json.dumps(test_res["choices"][0]["text"])
-        return r, True #test_res #r.text, True
+        print(r)
+        r = r.replace("\"",'')
+        print(r)
+
+        return r
 
 if __name__ == "__main__":
     res = query('What is the html code for a red button??')
