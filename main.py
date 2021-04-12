6e52afeeff333935df9e303d42c720146dbb5065
tonyarris <ash552@york.ac.uk>
2021-04-12 18:58:31 +0200

    Add extra examples to API call for image and background.

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ def gpt(prompt):
 
         # construct post request for answer
         headers = {'Content-type':'application/json','Authorization': 'Bearer {}'.format(OPENAI_API_KEY)}
-        data = json.dumps({"search_model": "ada",
+        data = json.dumps({"search_model": "davinci",
                            "model": "davinci",
                            "question": prompt,
                            "examples": [["A button with 'Submit' written on it", "<button type=\"button\">Submit</button>"],
-                                        ["A form to enter your name and surname and an enter button","<form action=\"/action_page.php\"><label for=\"fname\">First name:</label><br><input type=\"text\" id=\"fname\" name=\"fname\" value=\"John\"><br><label for=\"lname\">Last name:</label><br><input type=\"text\" id=\"lname\" name=\"lname\" value=\"Doe\"><br><br><input type=\"submit\" value=\"Submit\"></form>"]],
+                                        ["A form to enter your name and surname and an enter button","<form action=\"/action_page.php\"><label for=\"fname\">First name:</label><br><input type=\"text\" id=\"fname\" name=\"fname\" value=\"John\"><br><label for=\"lname\">Last name:</label><br><input type=\"text\" id=\"lname\" name=\"lname\" value=\"Doe\"><br><br><input type=\"submit\" value=\"Submit\"></form>"],
+                                        ["An image of the coca cola logo with a search bar underneath it.","<img src=\"https://img.icons8.com/color/452/coca-cola.png\"><br/><input type=\"text\" placeholder=\"Search..\">"],
+                                        ["A small Google logo, with a search bar underneath, all on a white background.", "<style>.auto {  background-color: white;}</style><div class=\"auto\"><img src=\"https://logo-logos.com/wp-content/uploads/2016/11/Google_logo_logotype.png\" width=\"300\"><br/><input type=\"text\" placeholder=\"Search..\"></div>"]],
                            "examples_context":"Generate the html and javascript code to realise the description given.",
                            "documents": [],
-                           "max_tokens": 250, 'temperature': 0.3, 'n': 1})
+                           "max_tokens": 250, 'temperature': 0.4, 'n': 1})
         r = requests.post('https://api.openai.com/v1/answers', headers=headers, data=data)
         #print(r.content)
 
